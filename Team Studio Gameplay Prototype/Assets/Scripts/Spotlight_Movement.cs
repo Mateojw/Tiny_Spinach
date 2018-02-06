5631b74011e5329775967a30bbed80ded6719fb1
Unknown <mjw557@nyu.edu>
2018-02-06 15:50:44 -0500

    FIrst Commit

---
+++ Team Studio Gameplay Prototype/Assets/Scripts/Spotlight_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight_Movement : MonoBehaviour {
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Input.acceleration.x, Input.acceleration.y, 0); // moving our object using the gyroscope
+		//transform.Translate (Input.acceleration.x, 0, 0); // no y
+		//rb.AddForce(new Vector2(Input.acceleration.x/10, 0));
+	}
+}
