9b9f88ca3c628829b1986145eb0ea22ad8b14c9e
Unknown <mjw557@nyu.edu>
2018-02-14 19:06:46 -0500

    adding on click to start but failed

--- Team Studio Gameplay Prototype/Assets/Scripts/Touch_To_Start.cs
+++ Team Studio Gameplay Prototype/Assets/Scripts/Touch_To_Start.cs
@@ -7,6 +7,7 @@ public class Touch_To_Start : MonoBehaviour {
 
 	public Spotlight_Movement Start_Spoghtlight;
 	public inside_Spotlight Start_Meter;
+	public Note Start_Note;
 
 	public GameObject Start_Text;
 
@@ -14,6 +15,7 @@ public class Touch_To_Start : MonoBehaviour {
 	void Start () {
 		Start_Spoghtlight.enabled = false;
 		Start_Meter.enabled = false;
+		Start_Note.enabled = false;
 		Start_Text.SetActive (true);
 	}
 	
@@ -22,7 +24,9 @@ public class Touch_To_Start : MonoBehaviour {
 
 		if (Input.GetMouseButton (0)) { // if the mouse button is down once
 			Start_Spoghtlight.enabled = true; // start the spoghtlight movemnet
-			Start_Meter.enabled = true;
+			Start_Meter.enabled = true; // start meter 
+			Start_Note.enabled = true; // start notes
+
 			Start_Text.SetActive (false);
 			Destroy (GetComponent<Touch_To_Start> ());
 		}
